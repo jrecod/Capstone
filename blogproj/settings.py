add309fc88b49c8d1921330f38dd634bf7dee8d5
jrecod <recodj@gmail.com>
2020-11-05 10:05:34 -0800

    changed static_root

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/uploaded_files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
